3c180ef4490e8a9b18f54cccb895b261cac456de
Aaron <aaron@amorgan.net.nz>
2021-11-06 11:31:46 +1300

    Code tidying/reformatting.

--- role.hauler.js
+++ role.hauler.js
@@ -2,23 +2,22 @@ var roleHauler = {
 
   /** @param {Creep} creep **/
   run: function (creep) {
-    if (creep.store.getFreeCapacity() > 0) {
-        var droppedResources = creep.room.find(FIND_DROPPED_RESOURCES);
-        
-        
-        var sorted = _.sortBy(droppedResources, 'energy');
-//        console.log("dr", sorted[sorted.length - 1].energy) // Do this better.
-        //var nearestDroppedSource = creep.pos.findClosestByPath(sorted[sorted.length - 1]);
-        
-        let nearestDroppedSource = sorted[sorted.length - 1];
-        
+    var stor = creep.room.storage;
+    console.log(JSON.stringify(stor))
 
-        if (nearestDroppedSource && creep.pickup(nearestDroppedSource) == ERR_NOT_IN_RANGE) {
-          creep.say('⚡  pickup ');
-          creep.moveTo(nearestDroppedSource, { visualizePathStyle: { stroke: '#ffaa00' } });
-        }
+    if (creep.store.getFreeCapacity() > 0) {
+      var droppedResources = creep.room.find(FIND_DROPPED_RESOURCES);
+      var sorted = _.sortBy(droppedResources, 'energy');
+      let nearestDroppedSource = sorted[sorted.length - 1];
+
+      if (nearestDroppedSource && creep.pickup(nearestDroppedSource) == ERR_NOT_IN_RANGE) {
+        creep.say('⚡  pickup ');
+        creep.moveTo(nearestDroppedSource, { visualizePathStyle: { stroke: '#ffaa00' } });
+      }
     }
     else {
+
+
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (structure.structureType == STRUCTURE_EXTENSION ||
@@ -28,12 +27,12 @@ var roleHauler = {
             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
         }
       });
-    
-        console.log('targets', targets);
 
       if (targets.length > 0) {
-          var dropSite = creep.pos.findClosestByPath(targets);
-          
+        var dropSite = creep.pos.findClosestByPath(targets);
+        // console.log('Hauler targeting:', JSON.stringify(dropSite))
+        console.log('Hauler target source:', dropSite);
+
         if (creep.transfer(dropSite, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
           creep.moveTo(dropSite, { visualizePathStyle: { stroke: '#ffffff' } });
         }
