8c1217508fb97a5a8b73ed12bcc547e6cc7d2353
Aaron <aaron@amorgan.net.nz>
2021-11-06 11:32:11 +1300

    Upgrader should check for containers before mining sources.

--- role.upgrader.js
+++ role.upgrader.js
@@ -17,26 +17,30 @@ var roleUpgrader = {
       }
     }
     else {
-           var targets = creep.room.find(FIND_STRUCTURES, {
-                  filter: (structure) => {
-                      return (structure.structureType == STRUCTURE_CONTAINER) && 
-                              structure.store.getUsedCapacity(RESOURCE_ENERGY) >= creep.store.getFreeCapacity();
-                  }
-          });
-          if(targets.length > 0) {
-              if(creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-              }
-          }
-          else {
-              let sources = creep.room.find(FIND_SOURCES);
-                let nearestSource = creep.pos.findClosestByPath(sources);      
+      var targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_CONTAINER) &&
+            structure.store.getUsedCapacity(RESOURCE_ENERGY) >= creep.store.getFreeCapacity();
+        }
+      });
+      if (targets.length > 0) {
+        var dropSite = creep.pos.findClosestByPath(targets);
 
-             if (creep.harvest(nearestSource) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(nearestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
-            }    
-          }
-          
+
+        console.log('Upgrader target source:', dropSite);
+        if (creep.withdraw(dropSite, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(dropSite, { visualizePathStyle: { stroke: '#ffffff' } });
+        }
+      }
+      else {
+        let sources = creep.room.find(FIND_SOURCES);
+        let nearestSource = creep.pos.findClosestByPath(sources);
+
+        if (creep.harvest(nearestSource) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(nearestSource, { visualizePathStyle: { stroke: '#ffaa00' } });
+        }
+      }
+
     }
   }
 };
