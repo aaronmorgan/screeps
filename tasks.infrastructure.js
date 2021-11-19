fa2217bd9f61deb0e5310a53af3ffe751d4dccf9
Aaron <aaron@amorgan.net.nz>
2021-11-19 15:30:42 +1300

    Add automated construction of the first tower.

--- tasks.infrastructure.js
+++ tasks.infrastructure.js
@@ -1,9 +1,26 @@
+
+// 
+//          r
+//          r
+//          rEE
+//          rTE
+//      rrrrSrrrr
+//          r
+//          r  C
+//          r
+//          r
+//
+
 var infrastructureTasks = {
 
   buildLinks: function (p_room) {
     let spawn = p_room.find(FIND_MY_STRUCTURES, {
       filter: { structureType: STRUCTURE_SPAWN }
     });
+
+    if (p_room.controller.level == 3) {
+      if (!p_room.memory.maxTowers) { p_room.memory.maxTowers = 1; }
+    }
 
     let s = spawn[0];
     let containers = p_room.find(FIND_STRUCTURES, { filter: (c) => c.structureType == STRUCTURE_CONTAINER });
@@ -15,21 +32,25 @@ var infrastructureTasks = {
 
         console.log('Construction of CONTAINER: ' + result);
         return;
-      // } else if (containers.length == 1) {
-      //   // TODO Check if there's anything at xy before attempting to build.
-      //   let result = p_room.createConstructionSite(s.pos.x - 3, s.pos.y - 2, "container");
+        // } else if (containers.length == 1) {
+        //   // TODO Check if there's anything at xy before attempting to build.
+        //   let result = p_room.createConstructionSite(s.pos.x - 3, s.pos.y - 2, "container");
 
-      //   console.log('Construction of CONTAINER: ' + result);
-      //   return;
+        //   console.log('Construction of CONTAINER: ' + result);
+        //   return;
       }
     }
+
+    let a = p_room.getPositionAt(s.pos.x + 2, s.pos.y - 1);
+    console.log('a', JSON.stringify(a));
+
+    let b = p_room.lookAt(a.x, a.y);
+    console.log('b', JSON.stringify(b));
 
     let extensions = p_room.find(FIND_STRUCTURES, { filter: (c) => c.structureType == STRUCTURE_EXTENSION });
 
     if (p_room.controller.level >= 2) {
       if (extensions.length < 3) {
-        let a = p_room.getPositionAt(s.pos.x + 2, s.pos.y - 1)
-
         // TODO Check if there's anything at xy before attempting to build.
         let result = p_room.createConstructionSite(s.pos.x + 3, s.pos.y - 2, "extension");
         if (result == ERR_INVALID_TARGET) {
@@ -45,11 +66,26 @@ var infrastructureTasks = {
     }
 
     if (p_room.controller.level >= 3) {
-      // TODO Check if there's anything at xy before attempting to build.
-      let result = p_room.createConstructionSite(s.pos.x + 2, s.pos.y - 1, "tower");
+      let towers = p_room.find(FIND_STRUCTURES, { filter: (c) => c.structureType == STRUCTURE_TOWER });
+
+      if (towers.length < p_room.memory.maxTowers) {
+
+        let b = p_room.lookAt(s.pos.x + 2, s.pos.y - 1);
+
+        if (b.length == 1 && b[0].type == 'terrain') {
+          let result = p_room.createConstructionSite(s.pos.x + 2, s.pos.y - 1, "tower");
+          console.log('Construction of TOWER: ' + result);
+        }
+
+        // for (var i = 0; i < b.length; i++) {
+        //   if (b[i].type == 'constructionSite') {
+
+        //   }
+        // }
 
 
-      console.log('Construction of TOWER: ' + result);
+      }
+
       return;
     }
 
