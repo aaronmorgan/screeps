f5afebaf9413e6779f48ff309dd5fa1e20cfb1be
Aaron <aaron@amorgan.net.nz>
2022-02-20 20:43:25 +1300

    Rework Infrastructure queuing algorithm.

---
+++ tasks.infrastructure.jobs.js
@@ -0,0 +1,297 @@
+module.exports.jobs = {
+    RCL_0: {
+        jobs: []
+    },
+    RCL_1: {
+        jobs: [{
+            type: "container",
+            x: 4,
+            y: 0
+        }]
+    },
+    RCL_2: {
+        jobs: [{
+                type: "extension",
+                x: 1,
+                y: -2
+            },
+            {
+                type: "extension",
+                x: 1,
+                y: -1
+            },
+            {
+                type: "extension",
+                x: 2,
+                y: -2
+            },
+            {
+                type: "extension",
+                x: 2,
+                y: -1
+            },
+            {
+                type: "extension",
+                x: 3,
+                y: -1
+            },
+            {
+                type: "container",
+                x: -4,
+                y: 0
+            },
+            // {
+            //     type: "rcl.container",
+            //     x: 0,
+            //     y: 0
+            // }
+        ]
+    },
+    RCL_3: {
+        jobs: [{
+                type: "tower",
+                x: 3,
+                y: -2
+            },
+            {
+                type: "extension",
+                x: -1,
+                y: -2
+            },
+            {
+                type: "extension",
+                x: -1,
+                y: -1
+            },
+            {
+                type: "extension",
+                x: -2,
+                y: -2
+            },
+            {
+                type: "extension",
+                x: -2,
+                y: -1
+            },
+            {
+                type: "extension",
+                x: -3,
+                y: -1
+            },
+            {
+                type: "container",
+                x: 0,
+                y: 3
+            },
+            // Roads, north, south, east, west
+            {
+                type: "road",
+                x: 1,
+                y: 0
+            },
+            {
+                type: "road",
+                x: 2,
+                y: 0
+            },
+            {
+                type: "road",
+                x: 3,
+                y: 0
+            },
+            {
+                type: "road",
+                x: 0,
+                y: 1
+            },
+            {
+                type: "road",
+                x: 0,
+                y: 2
+            },
+            {
+                type: "road",
+                x: -1,
+                y: 0
+            },
+            {
+                type: "road",
+                x: -2,
+                y: 0
+            },
+            {
+                type: "road",
+                x: -3,
+                y: 0
+            },
+            {
+                type: "road",
+                x: 0,
+                y: -1
+            },
+            {
+                type: "road",
+                x: 0,
+                y: -2
+            },
+            {
+                type: "road",
+                x: 1,
+                y: -3
+            },
+            {
+                type: "road",
+                x: 2,
+                y: -3
+            },
+            {
+                type: "road",
+                x: 3,
+                y: -3
+            },
+            {
+                type: "road",
+                x: 4,
+                y: -1
+            },
+            {
+                type: "road",
+                x: 4,
+                y: -2
+            },
+            {
+                type: "road",
+                x: 4,
+                y: 1
+            },
+            {
+                type: "road",
+                x: 4,
+                y: 2
+            },
+            {
+                type: "road",
+                x: 3,
+                y: 3
+            },
+            {
+                type: "road",
+                x: 2,
+                y: 3
+            },
+            {
+                type: "road",
+                x: 1,
+                y: 3
+            },
+            {
+                type: "road",
+                x: -1,
+                y: 3
+            },
+            {
+                type: "road",
+                x: -2,
+                y: 3
+            },
+            {
+                type: "road",
+                x: -3,
+                y: 3
+            },
+            {
+                type: "road",
+                x: -4,
+                y: 2
+            },
+            {
+                type: "road",
+                x: -4,
+                y: 1
+            },
+            {
+                type: "road",
+                x: -4,
+                y: -1
+            },
+            {
+                type: "road",
+                x: -4,
+                y: -2
+            },
+            {
+                type: "road",
+                x: -3,
+                y: -3
+            },
+            {
+                type: "road",
+                x: -2,
+                y: -3
+            },
+            {
+                type: "road",
+                x: -1,
+                y: -3
+            }
+        ]
+    },
+    RCL_4: {
+        jobs: [{
+                type: "extension",
+                x: 1,
+                y: 1
+            },
+            {
+                type: "extension",
+                x: 1,
+                y: 2
+            },
+            {
+                type: "extension",
+                x: 2,
+                y: 1
+            },
+            {
+                type: "extension",
+                x: 2,
+                y: 2
+            },
+            {
+                type: "extension",
+                x: 3,
+                y: 1
+            },
+            {
+                type: "extension",
+                x: -1,
+                y: 1
+            },
+            {
+                type: "extension",
+                x: -1,
+                y: 2
+            },
+            {
+                type: "extension",
+                x: -2,
+                y: 1
+            },
+            {
+                type: "extension",
+                x: -2,
+                y: 2
+            },
+            {
+                type: "extension",
+                x: -3,
+                y: 1
+            },
+            {
+                type: "storage",
+                x: 0,
+                y: -3
+            },
+        ]
+    },
+}
--- tasks.infrastructure.js
+++ tasks.infrastructure.js
@@ -3,52 +3,83 @@ const {
   structure
 } = require('game.constants');
 
+const {
+  jobs
+} = require('tasks.infrastructure.jobs');
+
 var infrastructureTasks = {
 
-  // Doesn't use a traditional queue or any cache but instead looks at current construction site objects
-  // to determine whether to continue or not.
-  buildLinks: function (p_room) {
-    // Only enqueue one construction site at a time.
-    const constructionSites = p_room.constructionSites();
-    if (constructionSites.length > 0) {
-      return;
-    }
 
-    const currentRCLLevel = p_room.controller.level;
+  processJobs: function (p_room, p_jobs) {
     const spawn = p_room.structures().spawn[0];
-    let index = 0; // p_room.controller.level;
 
-    // Periodically check whether we need to rebuild anything by resetting the construction job level.
-    // This could be further improved to increase the frequency to per tick during times of war.
-    // if (Game.time % (400 / p_room.controller.level) == 0) {
-    //   console.log('⚠️ Information: Resetting construction queue index to 0');
-    //   index = 0;
-    // }
+    for (let i = 0; i < p_jobs.length; i++) {
+      const job = p_jobs[i];
 
-    for (let i = index; i < constructionJobsTemplate.length; i++) {
-      const job = constructionJobsTemplate[i];
-      // console.log("Processing job: ", JSON.stringify(job));
+      let specialSite = false;
 
-      if (job.rclLevel > currentRCLLevel) {
-        if (currentRCLLevel == 3) {
-          p_room.sources().forEach(pos => {
-            let path = spawn.pos.findPathTo(pos, {
-              maxOps: 200
-            });
-            if (path.length) {
-              //console.log('path', JSON.stringify(path));
-              path.forEach(x => {
-                p_room.createConstructionSite(pos.x, pos.y, structure.STRUCTURE_ROAD);
-              });
-            }
-          });
+      let x = spawn.pos.x + job.x;
+      let y = spawn.pos.y + job.y;
 
-        }
-        break;
+      if (specialSite) {
+        x = job.x;
+        y = job.y;
       }
 
-      const x = spawn.pos.x + job.x;
-      const y = spawn.pos.y + job.y;
+      // if (job.type == 'rcl.container') {
+      //   // RCL adjacent container.
+      //   const path = spawn.pos.findPathTo(p_room.controller.pos, {
+      //     ignoreDestructibleStructures: true,
+      //     ignoreCreeps: true
+      //   });
+
+      //   if (path && path.length > 15) { // TODO This location positioning may need tweaking over time.
+      //     // Far enough away for Upgraders to have it at their backs while working
+      //     // but not so close that it gets in the way or too far that they have to 
+      //     // travel unnecessarily.
+
+      //     // TODO check that all surrounding tiles are empty and if not move 
+      //     // further away from the RCL until condition satisfied/
+      //     let pos = path[path.length - 7];
+
+      //     //p_room.createConstructionSite(pos.x, pos.y, structure.STRUCTURE_CONTAINER);
+      //     job = {
+      //       type: structure.STRUCTURE_CONTAINER,
+      //       x: pos.x,
+      //       y: pos.y
+      //     };
+
+      //     specialSite = true;
+      //   }
+      // }
+      // if (currentRCLLevel >= 3) {
+      //   p_room.sources().forEach(pos => {
+      //     let path = spawn.pos.findPathTo(pos, {
+      //       maxOps: 200,
+      //       ignoreCreeps: true
+      //     });
+      //     if (path.length) {
+      //       //console.log('path', JSON.stringify(path));
+      //        //path.forEach(x => {
+      //       //   p_room.createConstructionSite(pos.x, pos.y, structure.STRUCTURE_ROAD);
+      //       // });
+
+      //       job = {
+      //         type: structure.STRUCTURE_ROAD,
+      //         x: pos.x,
+      //         y: pos.y
+      //       };
+      //     }
+
+      //     specialSite = true;
+      //   });
+
+      //     }
+      //   break;
+      //     }
+
+
+
 
       const tileObjects = p_room.lookAt(x, y).filter(function (x) {
         return (
@@ -61,39 +92,6 @@ var infrastructureTasks = {
       if (tileObjects.length < 3 &&
         (tileObjects[0].type == 'terrain' && tileObjects[0].terrain != 'wall')) {
 
-        //console.log('terrain', JSON.stringify(tileObjects));
-
-        // switch (job.type) {
-        //   // Build a container near the RCL
-        //   case 'rcl.container': {
-        //     const path = p_room.findPath(spawn.pos, p_room.controller.pos, {
-        //       ignoreDestructibleStructures: true,
-        //       ignoreCreeps: true
-        //     });
-
-        //     if (path) {
-        //       console.log('tileobjects', JSON.stringify(tileObjects));
-        //       console.log('Information: Found a path to the RCL');
-
-        //       // Far enough away for Upgraders to have it at their backs while working
-        //       // but not so close that it gets in the way or too far that they have to 
-        //       // travel unnecessarily.
-        //       // TODO check that all surrounding tiles are empty and if not move 
-        //       // further away from the RCL until condition satisfied/
-        //       let newPos = path[path.length - 5];
-        //       console.log('newPos', JSON.stringify(newPos));
-        //       job.type = 'container';
-        //       x = newPos.x;
-        //       y = newPos.y;
-
-        //       console.log(JSON.stringify(job))
-
-        //     } else {
-        //       console.log('⛔ Error: calling createConstructionSite for rcl.container, ' + EXIT_CODE[result]);
-        //     }
-        //   }
-        // }
-
         let result = p_room.createConstructionSite(x, y, job.type);
 
         switch (result) {
@@ -103,12 +101,16 @@ var infrastructureTasks = {
           }
           default: {
             console.log('⛔ Error: calling createConstructionSite, ' + EXIT_CODE[result] + ', job=', JSON.stringify(job) + ', x=' + job.x + ', y=' + job.y);
-            continue;
+            return;
           }
         }
       } else {
         let tile = tileObjects[0];
 
+        if (tile.type == 'constructionSite') {
+          continue;
+        }
+        //  console.log('tile', JSON.stringify(tile));
         if (!tile.structure) {
           console.log('⛔ Error: ' + JSON.stringify(tile));
 
@@ -120,353 +122,27 @@ var infrastructureTasks = {
           console.log('⚠️ WARNING: Position x: ' + tile.structure.pos.x + ', y: ' + tile.structure.pos.y + ', is already allocated with a ' + tile.structure.structureType);
         }
       }
-    };
+    }
   },
-}
 
-const constructionJobsTemplate = [
-  // Level 0: Roads, 5 Containers
-  {
-    rclLevel: 0,
-    type: "container",
-    x: 4,
-    y: 0
-  },
-  // Level 2
-  {
-    rclLevel: 2,
-    type: "extension",
-    x: 1,
-    y: -2
-  },
-  {
-    rclLevel: 2,
-    type: "extension",
-    x: 1,
-    y: -1
-  },
-  {
-    rclLevel: 2,
-    type: "extension",
-    x: 2,
-    y: -2
-  },
-  {
-    rclLevel: 2,
-    type: "extension",
-    x: 2,
-    y: -1
-  },
-  {
-    rclLevel: 2,
-    type: "extension",
-    x: 3,
-    y: -1
-  },
-  {
-    rclLevel: 2,
-    type: "container",
-    x: -4,
-    y: 0
-  },
-  // Level 3
-  {
-    rclLevel: 3,
-    type: "tower",
-    x: 3,
-    y: -2
-  },
-  {
-    rclLevel: 3,
-    type: "extension",
-    x: -1,
-    y: -2
-  },
-  {
-    rclLevel: 3,
-    type: "extension",
-    x: -1,
-    y: -1
-  },
-  {
-    rclLevel: 3,
-    type: "extension",
-    x: -2,
-    y: -2
-  },
-  {
-    rclLevel: 3,
-    type: "extension",
-    x: -2,
-    y: -1
-  },
-  {
-    rclLevel: 3,
-    type: "extension",
-    x: -3,
-    y: -1
-  },
-  {
-    rclLevel: 3,
-    type: "container",
-    x: 0,
-    y: 3
-  },
-  // Roads, north, south, east, west
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 1,
-    y: 0
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 2,
-    y: 0
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 3,
-    y: 0
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 0,
-    y: 1
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 0,
-    y: 2
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -1,
-    y: 0
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -2,
-    y: 0
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -3,
-    y: 0
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 0,
-    y: -1
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 0,
-    y: -2
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 1,
-    y: -3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 2,
-    y: -3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 3,
-    y: -3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 4,
-    y: -1
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 4,
-    y: -2
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 4,
-    y: 1
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 4,
-    y: 2
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 3,
-    y: 3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 2,
-    y: 3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: 1,
-    y: 3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -1,
-    y: 3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -2,
-    y: 3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -3,
-    y: 3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -4,
-    y: 2
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -4,
-    y: 1
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -4,
-    y: -1
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -4,
-    y: -2
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -3,
-    y: -3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -2,
-    y: -3
-  },
-  {
-    rclLevel: 3,
-    type: "road",
-    x: -1,
-    y: -3
-  },
-  // Level 4
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: 1,
-    y: 1
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: 1,
-    y: 2
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: 2,
-    y: 1
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: 2,
-    y: 2
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: 3,
-    y: 1
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: -1,
-    y: 1
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: -1,
-    y: 2
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: -2,
-    y: 1
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: -2,
-    y: 2
-  },
-  {
-    rclLevel: 4,
-    type: "extension",
-    x: -3,
-    y: 1
-  },
-  {
-    rclLevel: 4,
-    type: "storage",
-    x: 0,
-    y: -3
-  },
-  // Level 5
-  {
-    rclLevel: 5,
-    type: "tower",
-    x: -3,
-    y: -2
-  },
-];
+  // Doesn't use a traditional queue or any cache but instead looks at current construction site objects
+  // to determine whether to continue or not.
+  buildLinks: function (p_room) {
+    // Only enqueue one construction site at a time.
+    const constructionSites = p_room.constructionSites();
+    if (constructionSites.length > 0) {
+      return;
+    }
+
+    // Periodically check whether we need to rebuild anything by resetting the construction job level.
+    // This could be further improved to increase the frequency to per tick during times of war.
+    // if (Game.time % (400 / p_room.controller.level) == 0) {
+    //   console.log('⚠️ Information: Resetting construction queue index to 0');
+    //   index = 0;
+    // }
+
+    this.processJobs(p_room, jobs['RCL_' + p_room.controller.level].jobs);
+  }
+}
 
 module.exports = infrastructureTasks;
