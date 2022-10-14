15efe7526d8ddf23d3c7ba1cb6a81cdacf42c8de
Aaron <aaron@amorgan.net.nz>
2022-10-14 17:37:18 +1300

    Dropminers spawn with CARRY part if their target Source has a Link structure.

--- role.dropminer.js
+++ role.dropminer.js
@@ -10,29 +10,42 @@ let creepFactory = require('tasks.build.creeps');
 var roleDropMiner = {
 
     tryBuild: function (p_spawn, p_energyCapacityAvailable) {
+        const targetSourceId = p_spawn.room.selectAvailableSource(p_spawn.room.creeps().dropminers)[0].id;
+        const source = Game.getObjectById(targetSourceId);
+
+        const linkStructure = Game.getObjectById(source.id).pos.findInRange(FIND_MY_STRUCTURES, 3, {
+            filter: {
+                structureType: STRUCTURE_LINK
+            }
+        })[0];
+
+        let lastPart = MOVE;
+
+        if (linkStructure) {
+            lastPart = CARRY;
+        }
+
+
         let bodyType = [];
 
         if (p_energyCapacityAvailable >= 700) {
-            bodyType = [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE];
+            bodyType = [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, lastPart];
             p_spawn.room.memory.minersPerSource = 1;
         } else if (p_energyCapacityAvailable >= 600) {
-            bodyType = [WORK, WORK, WORK, WORK, WORK, MOVE, MOVE]; // 5 WORK parts mine exactly 3000 energy every 300 ticks.
+            bodyType = [WORK, WORK, WORK, WORK, WORK, MOVE, lastPart]; // 5 WORK parts mine exactly 3000 energy every 300 ticks.
             p_spawn.room.memory.minersPerSource = 1;
         } else if (p_energyCapacityAvailable >= 400) {
-            bodyType = [WORK, WORK, WORK, MOVE, MOVE];
+            bodyType = [WORK, WORK, WORK, MOVE, lastPart];
             p_spawn.room.memory.minersPerSource = 2;
         } else if (p_energyCapacityAvailable >= 350) {
-            bodyType = [WORK, WORK, WORK, MOVE];
+            bodyType = [WORK, WORK, WORK, lastPart];
             p_spawn.room.memory.minersPerSource = 2;
         } else {
-            bodyType = [WORK, WORK, MOVE, MOVE];
+            bodyType = [WORK, WORK, MOVE, lastPart];
             p_spawn.room.memory.minersPerSource = 2;
         }
 
         if (!_.isEmpty(bodyType)) {
-            // Potential bug: We could end up with a dropminer assigning itself to a source with a remaining harvester.
-            const targetSourceId = p_spawn.room.selectAvailableSource(p_spawn.room.creeps().dropminers)[0].id;
-
             if (!targetSourceId) {
                 console.log('ERROR: Attempting to create ' + role.DROPMINER + ' with an assigned source');
                 return EXIT_CODE.ERR_INVALID_TARGET;
