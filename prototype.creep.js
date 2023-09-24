6464b69bfb654a80a3a9615396c72694321eff2c
Aaron <aaron@amorgan.net.nz>
2023-09-24 13:08:04 +1300

    Fix bug where findEnergyTransferTarget could get a null findClosestByPath() result if pathing blocked by other creeps and room boundary.

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,5 +1,4 @@
 module.exports = function () {
-
     Creep.prototype.checkTicksToLive = function () {
         if (this.ticksToLive == 1) {
             this.dropResourcesAndDie();
@@ -25,7 +24,12 @@ module.exports = function () {
 
     Creep.prototype.dropResourcesAndDie = function () {
         for (const resourceType in this.carry) {
-            console.log('💀 TTD Creep: ' + this.name + ', dropping resource: ' + resourceType)
+            console.log(
+                "💀 TTD Creep: " +
+                    this.name +
+                    ", dropping resource: " +
+                    resourceType
+            );
             this.drop(resourceType);
         }
 
@@ -35,38 +39,74 @@ module.exports = function () {
     Creep.prototype.findEnergyTransferTarget = function () {
         let targets = [];
 
-        if (Game.spawns['Spawn1'].store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-            targets.push(Game.spawns['Spawn1']);
+        if (Game.spawns["Spawn1"].store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+            targets.push(Game.spawns["Spawn1"]);
         }
         if (targets.length == 0) {
-            targets = _.filter(this.room.structures().storage, (structure) => structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+            targets = _.filter(
+                this.room.structures().storage,
+                (structure) =>
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            );
         }
-        // It gets into a state where the Tower is consuming more energy than can be supplied, creating a feedback loop 
+        // It gets into a state where the Tower is consuming more energy than can be supplied, creating a feedback loop
         // where creeps die and structures decay.
         if (targets.length == 0 && this.room.creeps().dropminers.length > 1) {
             // Only refill the Tower if the fill percentage is < 20%.
-            targets = _.filter(this.room.structures().tower, (structure) => Math.round(structure.store.getUsedCapacity(RESOURCE_ENERGY) / structure.store.getCapacity(RESOURCE_ENERGY) * 100) < 80);
+            targets = _.filter(
+                this.room.structures().tower,
+                (structure) =>
+                    Math.round(
+                        (structure.store.getUsedCapacity(RESOURCE_ENERGY) /
+                            structure.store.getCapacity(RESOURCE_ENERGY)) *
+                            100
+                    ) < 80
+            );
         }
         if (targets.length == 0) {
-            targets = _.filter(this.room.structures().extension, (structure) => structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+            targets = _.filter(
+                this.room.structures().extension,
+                (structure) =>
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            );
         }
         if (targets.length == 0) {
-            targets = _.filter(this.room.structures().storage, (structure) => structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+            targets = _.filter(
+                this.room.structures().storage,
+                (structure) =>
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            );
         }
         if (targets.length == 0) {
-            targets = _.filter(this.room.structures().container, (structure) => structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0);
+            targets = _.filter(
+                this.room.structures().container,
+                (structure) =>
+                    structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            );
         }
 
         if (targets.length > 0) {
-            const target = this.pos.findClosestByPath(targets)
+            let target = this.pos.findClosestByPath(targets);
+            if (target == null) {
+                target = this.pos.findClosestByRange(targets);
+            }
+
             const transferResult = this.transfer(target, RESOURCE_ENERGY);
 
+            switch (transferResult) {
+                case ERR_INVALID_TARGET: 
+                case ERR_NOT_IN_RANGE: {
+                    const moveResult = this.moveTo(target, {
+                        visualizePathStyle: {
+                            stroke: "#ffffff",
+                        },
+                    });
+
+                    break;
+                }
+            }
+
             if (transferResult == ERR_NOT_IN_RANGE) {
-                this.moveTo(target, {
-                    visualizePathStyle: {
-                        stroke: '#ffffff'
-                    }
-                });
             }
         }
 
@@ -74,7 +114,8 @@ module.exports = function () {
     };
 
     Creep.prototype.CreepFillPercentage = function () {
-        return Math.round(this.store.getUsedCapacity() / this.store.getCapacity() * 100);
+        return Math.round(
+            (this.store.getUsedCapacity() / this.store.getCapacity()) * 100
+        );
     };
-
 };
