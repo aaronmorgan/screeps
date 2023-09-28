ee28ede6376187ee6002335e685ae90d480c373e
Aaron <aaron@amorgan.net.nz>
2023-09-28 20:04:43 +1300

    Further updates to the infrastructure layout.

--- tasks.infrastructure.jobs.js
+++ tasks.infrastructure.jobs.js
@@ -1,5 +1,5 @@
 // Reserved tiles:
-// x: 0, y: 3 - energy dumping spot.
+// x: 0, y: 4 - energy dumping spot.
 
 module.exports.jobs = {
     RCL_0: {
@@ -29,6 +29,8 @@ module.exports.jobs = {
             { type: "road", x: 1, y: 2 },
             { type: "road", x: 2, y: -2 },
             { type: "road", x: -1, y: -2 },
+            { type: "road", x: -2, y: -2 },
+            { type: "road", x: -3, y: -3 },
             // Main roads.
             { type: "road.to.controller", x: 0, y: 0 },
             { type: "road.to.source", x: 0, y: 0 },
@@ -39,105 +41,99 @@ module.exports.jobs = {
     RCL_3: {
         jobs: [
             { type: "tower", x: 3, y: -2 },
+
             { type: "extension", x: 2, y: -1 },
             { type: "extension", x: 0, y: -2 },
             { type: "extension", x: 1, y: -2 },
             { type: "extension", x: -2, y: 3 },
             { type: "extension", x: -1, y: 3 },
-            { type: "container", x: 1, y: 3 },
+
+            // { type: "container", x: 1, y: 3 },
+            { type: "road", x: 4, y: -2 },
+            { type: "road", x: 4, y: -1 },
+            { type: "road", x: 4, y: 0 },
+            { type: "road", x: 4, y: 1 },
+            { type: "road", x: 4, y: 2 },
+            { type: "road", x: 3, y: 3 },
+
+            { type: "road", x: 2, y: 4 },
+            { type: "road", x: 1, y: 4 },
+            { type: "road", x: 0, y: 4 },
+            { type: "road", x: -1, y: 4 },
+            { type: "road", x: -2, y: 4 },
+
+            { type: "road", x: 2, y: -4 },
+            { type: "road", x: 1, y: -4 },
+            { type: "road", x: 0, y: -4 },
+            { type: "road", x: -1, y: -4 },
+            { type: "road", x: -2, y: -4 },
+
+            { type: "road", x: -4, y: 2 },
+            { type: "road", x: -4, y: 1 },
+            { type: "road", x: -4, y: 0 },
+            { type: "road", x: -4, y: -1 },
+            { type: "road", x: -4, y: -2 },
         ]
     },
     RCL_4: {
         jobs: [
-            { type: "extension", x: 2, y: -1 },
-            { type: "extension", x: 2, y: -2 },
+            { type: "extension", x: 1, y: 3 },
+            { type: "extension", x: 2, y: 0 },
+            { type: "extension", x: 2, y: 1 },
+            { type: "extension", x: 2, y: 2 },
             { type: "extension", x: 3, y: -1 },
             { type: "extension", x: 3, y: 0 },
             { type: "extension", x: 3, y: 1 },
+
+            { type: "storage", x: 0, y: -3 },
+        ]
+    },
+    RCL_5: {
+        jobs: [
+            { type: "tower", x: -3, y: 2 },
+            { type: "link", x: 0, y: -4 },
+            { type: "source.link", x: 0, y: 0 },
+
+            // {
+            //     type: "road",
+            //     x: 5,
+            //     y: 0
+            // },
+            // {
+            //     type: "road",
+            //     x: 0,
+            //     y: 4
+            // },
+            // {
+            //     type: "road",
+            //     x: -5,
+            //     y: 0
+            // },
+            // {
+            //     type: "road",
+            //     x: 0,
+            //     y: -5
+            // },
+            // {
+            //     type: "road",
+            //     x: 1,
+            //     y: -4
+            // },
+            // {
+            //     type: "road",
+            //     x: -1,
+            //     y: -4
+            // },
             {
                 type: "extension",
-                x: -1,
-                y: 1
-            },
-            {
-                type: "extension",
-                x: -1,
-                y: 2
-            },
-            {
-                type: "extension",
-                x: -2,
-                y: 1
-            },
-            {
-                type: "extension",
-                x: -2,
+                x: 3,
                 y: 2
             },
             {
                 type: "extension",
                 x: -3,
-                y: 1
-            },
-            { type: "storage", x: 0, y: -3 },
-        ]
-    },
-    RCL_5: {
-        jobs: [{
-            type: "tower",
-            x: -3,
-            y: 2
-        },
-        {
-            type: "link",
-            x: 0,
-            y: -4
-        },
-        {
-            type: "source.link",
-            x: 0,
-            y: 0
-        },
-        // {
-        //     type: "road",
-        //     x: 5,
-        //     y: 0
-        // },
-        // {
-        //     type: "road",
-        //     x: 0,
-        //     y: 4
-        // },
-        // {
-        //     type: "road",
-        //     x: -5,
-        //     y: 0
-        // },
-        // {
-        //     type: "road",
-        //     x: 0,
-        //     y: -5
-        // },
-        // {
-        //     type: "road",
-        //     x: 1,
-        //     y: -4
-        // },
-        // {
-        //     type: "road",
-        //     x: -1,
-        //     y: -4
-        // },
-        {
-            type: "extension",
-            x: 3,
-            y: 2
-        },
-        {
-            type: "extension",
-            x: -3,
-            y: -2
-        }
+                y: -2
+            }
         ]
     },
     RCL_6: {
